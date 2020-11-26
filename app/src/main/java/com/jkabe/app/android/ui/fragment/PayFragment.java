7b5d454da9eb0f2199765096b975dfb2a717f82c
aq610428 <641897605@qq.com>
2020-11-26 14:34:04 +0800

    111111111111

--- app/src/main/java/com/jkabe/app/android/ui/fragment/PayFragment.java
+++ app/src/main/java/com/jkabe/app/android/ui/fragment/PayFragment.java
@@ -11,7 +11,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.LinearLayoutManager;
